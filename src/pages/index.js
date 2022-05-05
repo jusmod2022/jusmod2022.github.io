8ceddd33c55616bd38000d87f2cecb061e44ef0a
Mattia <mattia@MacBook-Air-di-Mattia.local>
2022-05-05 17:24:01 +0200

    keynote update

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,7 +142,7 @@ export default function Home() {
           <h3>Keynote</h3>
           
           <ul>
-            <li><b>Enrico Francesconi</b></li>
+            <li><b>href="https://www.linkedin.com/in/enrico-francesconi-4754958/"> Enrico Francesconi</b></li>
             <p>
             Title:
             Legal Knowledge Representation and Reasoning in the Semantic Web         
